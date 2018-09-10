958c4da0eafe092dd965082f5432cc64118bebb2
Matt Johnson <mjohnson541@gmail.com>
2018-09-10 11:37:35 -0400

    add a leave-one-out option for the cross-validation functions

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -2950,12 +2950,14 @@ class KineticsFamily(Database):
         iters times.  
         Returns a dictionary mapping {rxn:Ln(k_Est/k_Train)}
         """
-        
         if templateRxnMap is None:
             templateRxnMap = self.getReactionMatches(removeDegeneracy=True,getReverse=True)
         
         rxns = np.array(templateRxnMap['Root'])
         
+        if folds == 0:
+            folds = len(rxns)
+            
         kf = KFold(folds,shuffle=True,random_state=random_state)
         errors = {}
         
@@ -3005,10 +3007,13 @@ class KineticsFamily(Database):
         Perform K-fold cross validation on an automatically generated tree at temperature T
         Returns a dictionary mapping {rxn:Ln(k_Est/k_Train)}
         """
-        
-        kf = KFold(folds,shuffle=True,random_state=random_state)
         errors = {}
         rxns = np.array(self.getTrainingSet(removeDegeneracy=True))
+        
+        if folds == 0:
+            folds = len(rxns)
+            
+        kf = KFold(folds,shuffle=True,random_state=random_state)
         
         if thermoDatabase is None:
             from rmgpy.data.rmg import getDB
